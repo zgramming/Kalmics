54bdb41a4a1e551542afea796c4a0f4b9d518976
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-22 02:14:32 +0700

    global Provider for loading,search & animation

--- lib/src/provider/global/global_provider.dart
+++ lib/src/provider/global/global_provider.dart
@@ -1,9 +1,19 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 
 /// For loading builder in ProviderListener
-final isLoading = StateProvider.autoDispose<bool>((ref) => false);
+final isLoading = StateProvider<bool>((ref) => false);
+
+/// Animation Controller MusicPlayerDetailScreen
+final globalSizeAnimationController = StateProvider<AnimationController?>((ref) => null);
+
+/// Handling Toggle Search & TextFormField
+final globalSearch = StateProvider.autoDispose<bool>((ref) => false);
+
+/// Handling query Search
+final searchQuery = StateProvider<String>((ref) => '');
 
 /// Initialize Class AudioPlayers
 final globalAudioPlayers = StateProvider<AssetsAudioPlayer>((ref) {
