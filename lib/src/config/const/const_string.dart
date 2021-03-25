b5768af45c2f4aa56b4a85b8aaa54a7043d67c0b
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-25 14:34:07 +0700

    add new const string

--- lib/src/config/const/const_string.dart
+++ lib/src/config/const/const_string.dart
@@ -1,4 +1,10 @@
 class ConstString {
   ///* Internal Storage platform android
   static const String androidPathStorage = '/storage/emulated/0/';
+  static const String excludePathFile = '/android/data/';
+
+  ///* Sorting Choice
+  static const String sortChoiceByTitle = 'title';
+  static const String sortChoiceByArtist = 'artist';
+  static const String sortChoiceByDuration = 'duration';
 }
