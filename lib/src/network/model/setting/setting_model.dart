365c1d8b3c9c72799929292838bd69f3e8c4e0b5
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-25 14:34:58 +0700

    add property [sortByType,sortChoice]

--- lib/src/network/model/setting/setting_model.dart
+++ lib/src/network/model/setting/setting_model.dart
@@ -1,23 +1,32 @@
 import 'package:equatable/equatable.dart';
 
+enum SortByType { ascending, descending }
+
 class SettingModel extends Equatable {
   final bool isPassedOnboarding;
-
+  final SortByType sortByType;
+  final String sortChoice;
   const SettingModel({
     this.isPassedOnboarding = false,
+    this.sortByType = SortByType.ascending,
+    this.sortChoice = 'title',
   });
 
   @override
-  List get props => [isPassedOnboarding];
+  List<Object> get props => [isPassedOnboarding, sortByType, sortChoice];
 
   @override
   bool get stringify => true;
 
   SettingModel copyWith({
     bool? isPassedOnboarding,
+    SortByType? sortByType,
+    String? sortChoice,
   }) {
     return SettingModel(
       isPassedOnboarding: isPassedOnboarding ?? this.isPassedOnboarding,
+      sortByType: sortByType ?? this.sortByType,
+      sortChoice: sortChoice ?? this.sortChoice,
     );
   }
 }
