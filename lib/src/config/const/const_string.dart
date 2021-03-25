2dbc499392cb996ad0c10b771733c2840a1a70e1
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-26 01:12:24 +0700

    new const string

--- lib/src/config/const/const_string.dart
+++ lib/src/config/const/const_string.dart
@@ -1,10 +1,15 @@
 class ConstString {
   ///* Internal Storage platform android
-  static const String androidPathStorage = '/storage/emulated/0/';
+  static const String internalPathStorageAndroid = '/storage/emulated/0/';
   static const String excludePathFile = '/android/data/';
 
   ///* Sorting Choice
   static const String sortChoiceByTitle = 'title';
   static const String sortChoiceByArtist = 'artist';
   static const String sortChoiceByDuration = 'duration';
+
+  ///* PopuMenuButton Value
+  static const String syncPMB = 'sync';
+  static const String sortPMB = 'sort';
+  static const String timerPMB = 'timer';
 }
