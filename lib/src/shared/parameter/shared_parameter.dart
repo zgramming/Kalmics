593e30026c7e2954a27d480ff916bef14ab4c25e
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-25 14:36:01 +0700

    check null variable

--- lib/src/shared/parameter/shared_parameter.dart
+++ lib/src/shared/parameter/shared_parameter.dart
@@ -1,17 +1,19 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:global_template/global_template.dart';
+
 import 'package:kalmics/src/network/my_network.dart';
 import 'package:kalmics/src/provider/my_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SharedParameter {
   Metas metas(MusicModel music) {
     return Metas(
-      album: music.tag?.album,
-      artist: music.tag?.artist,
       id: music.idMusic,
-      title: music.tag?.title,
+
+      album: (music.tag?.album?.isNotEmpty ?? false) ? music.tag?.album : 'Unknown Album',
+      artist: (music.tag?.artist?.isNotEmpty ?? false) ? music.tag?.artist : 'Unknown Artist',
+      title: (music.title?.isNotEmpty ?? false) ? music.title : 'Unknown Title',
       // image: MetasImage.file(File.fromRawPath(music.artwork ?? Uint8List.fromList([])).path),
       onImageLoadFail: MetasImage.asset('${appConfig.urlImageAsset}/${appConfig.nameLogoAsset}'),
     );
