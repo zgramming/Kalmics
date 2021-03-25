aac8898aa6e67bc72277c5ea35853213b3bab7b2
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-25 14:33:56 +0700

    handle notification music action

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:hive/hive.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:global_template/global_template.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:kalmics/src/provider/my_provider.dart';
+
 import 'src/app.dart';
 
 const musicBox = 'musicBox';
@@ -13,8 +17,20 @@ Future<void> main() async {
   initializeDateFormatting();
   appConfig.configuration(nameLogoAsset: 'Kalmics.png');
 
+  AssetsAudioPlayer.setupNotificationsOpenAction((notification) {
+    //custom action
+    return true; //true : handled, does not notify others listeners
+    //false : enable others listeners to handle it
+  });
+
+  AssetsAudioPlayer.addNotificationOpenAction((notification) {
+    //custom action
+    return false; //true : handled, does not notify others listeners
+    //false : enable others listeners to handle it
+  });
+
   await Hive.initFlutter();
-  await Hive.openBox(musicBox);
+  await Hive.openBox(SettingProvider.boxSettingKey);
   colorPallete.configuration(
     primaryColor: const Color(0xFF52057b),
     accentColor: const Color(0xFFA70071),
