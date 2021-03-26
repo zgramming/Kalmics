516be11586e6b4a2631abeed7be51b3594882aa8
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-26 19:57:42 +0700

    new property [isShuffle,loopMode,timerDuration]

--- lib/src/network/model/setting/setting_model.dart
+++ lib/src/network/model/setting/setting_model.dart
@@ -1,36 +1,54 @@
 import 'package:equatable/equatable.dart';
 
 enum SortByType { ascending, descending }
+enum LoopModeSetting { all, single, none }
 
 class SettingModel extends Equatable {
   final bool isPassedOnboarding;
+  final bool isShuffle;
+  final LoopModeSetting loopMode;
+  final Duration timerDuration;
   final SortByType sortByType;
   final String sortChoice;
-  final Duration timerDuration;
   const SettingModel({
     this.isPassedOnboarding = false,
+    this.isShuffle = false,
+    this.loopMode = LoopModeSetting.all,
+    this.timerDuration = Duration.zero,
     this.sortByType = SortByType.ascending,
     this.sortChoice = 'title',
-    this.timerDuration = Duration.zero,
   });
 
   @override
-  List<Object> get props => [isPassedOnboarding, sortByType, sortChoice, timerDuration];
+  List<Object> get props {
+    return [
+      isPassedOnboarding,
+      isShuffle,
+      loopMode,
+      timerDuration,
+      sortByType,
+      sortChoice,
+    ];
+  }
 
   @override
   bool get stringify => true;
 
   SettingModel copyWith({
     bool? isPassedOnboarding,
+    bool? isShuffle,
+    LoopModeSetting? loopMode,
+    Duration? timerDuration,
     SortByType? sortByType,
     String? sortChoice,
-    Duration? timerDuration,
   }) {
     return SettingModel(
       isPassedOnboarding: isPassedOnboarding ?? this.isPassedOnboarding,
+      isShuffle: isShuffle ?? this.isShuffle,
+      loopMode: loopMode ?? this.loopMode,
+      timerDuration: timerDuration ?? this.timerDuration,
       sortByType: sortByType ?? this.sortByType,
       sortChoice: sortChoice ?? this.sortChoice,
-      timerDuration: timerDuration ?? this.timerDuration,
     );
   }
 }
