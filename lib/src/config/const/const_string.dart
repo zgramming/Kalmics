137a517b08932047cb95a477afda1ef3ef068537
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-26 19:55:24 +0700

    update const string

--- lib/src/config/const/const_string.dart
+++ lib/src/config/const/const_string.dart
@@ -12,4 +12,21 @@ class ConstString {
   static const String syncPMB = 'sync';
   static const String sortPMB = 'sort';
   static const String timerPMB = 'timer';
+
+  ///* LoopMode Value
+  static const String loopModeAll = 'all';
+  static const String loopModelSingle = 'single';
+  static const String loopModelNone = 'none';
+
+  ///* Ascending & Descending Value
+  static const int ascendingValue = 0;
+  static const int descendingValue = 1;
+
+  ///* Onboarding Value
+  static const bool finishedOnboarding = true;
+  static const bool notFinishedOnboarding = false;
+
+  ///* Shuffle Value
+  static const bool useShuffle = true;
+  static const bool notUseShuffle = false;
 }
