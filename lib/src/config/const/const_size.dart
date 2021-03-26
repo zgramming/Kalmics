128fb6b2bf016b3c0ef871b383cf3191b3120938
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-03-26 19:55:13 +0700

    update const size

--- lib/src/config/const/const_size.dart
+++ lib/src/config/const/const_size.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:global_template/global_template.dart';
+
 class ConstSize {
   ///* Screen : MusicPlayerDetail
 
@@ -6,7 +9,9 @@ class ConstSize {
   ///* Widget : [music_player_detail_action_previous]
   ///* Widget : [music_player_detail_action_repeat]
   ///* Widget : [music_player_detail_action_shuffle]
-  static const double iconActionMusicPlayerDetail = 9.0;
+  static double iconActionMusicPlayerDetail(BuildContext context) => sizes.width(context) / 12;
+  static double radiusIconActionMusicPlayerDetail(BuildContext context) =>
+      sizes.width(context) / 15;
 
   ///* Widget : [music_player_detail_action]
   static const double spacingIconMusicPlayerDetail = 10.0;
