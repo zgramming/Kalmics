dec8399269213a3aefa3f0c29962343e80d5b287
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-04-05 14:42:44 +0700

    improved animation onboarding template

--- lib/src/screen/onboarding/onboarding_screen.dart
+++ lib/src/screen/onboarding/onboarding_screen.dart
@@ -14,34 +14,6 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerProviderStateMixin {
-  late final AnimationController animationController;
-  late final Animation<Offset> translateAnimation;
-  late final Animation<double> scaleAnimation;
-  late final Animation<double> rotateAnimation;
-
-  @override
-  void initState() {
-    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
-
-    translateAnimation = Tween<Offset>(begin: const Offset(200, 0), end: const Offset(0, 0))
-        .animate(CurvedAnimation(parent: animationController, curve: const Interval(0.1, 1)));
-
-    scaleAnimation = Tween<double>(begin: 1, end: 1.5)
-        .animate(CurvedAnimation(parent: animationController, curve: const Interval(0.7, 1)));
-
-    rotateAnimation = Tween<double>(begin: 0, end: -0.25)
-        .animate(CurvedAnimation(parent: animationController, curve: const Interval(0.9, 1)));
-
-    animationController.forward();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,10 +32,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
             end: Alignment.bottomRight,
             colors: ConstColor.backgroundColorGradient(),
           ),
-          onPageChanged: (index) {
-            animationController.reset();
-            animationController.forward();
-          },
+          onPageChanged: (index) {},
           onClickNext: (index) {},
           onClickFinish: () async {
             final permissionStorage = await Permission.storage.status;
@@ -104,12 +73,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
             OnboardingItem(
               logo: Align(
                 alignment: Alignment.bottomCenter,
-                child: _AnimatedBuilder(
-                  animationController: animationController,
-                  translateAnimation: translateAnimation,
-                  scaleAnimation: scaleAnimation,
-                  rotateAnimation: rotateAnimation,
-                  icon: Icons.music_note,
+                child: Icon(
+                  Icons.music_note,
+                  size: sizes.width(context) / 2,
+                  color: Colors.white,
                 ),
               ),
               title: 'Tampilan Musik Kekinian',
@@ -120,12 +87,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
               ),
             ),
             OnboardingItem(
-              logo: _AnimatedBuilder(
-                animationController: animationController,
-                translateAnimation: translateAnimation,
-                scaleAnimation: scaleAnimation,
-                rotateAnimation: rotateAnimation,
-                icon: Icons.tag,
+              logo: Icon(
+                Icons.tag,
+                size: sizes.width(context) / 2,
+                color: Colors.white,
               ),
               title: 'Fitur-fitur yang menarik',
               titleStyle: GoogleFonts.montserrat(
@@ -135,12 +100,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
               ),
             ),
             OnboardingItem(
-              logo: _AnimatedBuilder(
-                animationController: animationController,
-                translateAnimation: translateAnimation,
-                scaleAnimation: scaleAnimation,
-                rotateAnimation: rotateAnimation,
-                icon: Icons.play_arrow_rounded,
+              logo: Icon(
+                Icons.play_arrow_rounded,
+                size: sizes.width(context) / 2,
+                color: Colors.white,
               ),
               title: 'Mainkan lagu pertamamu',
               titleStyle: GoogleFonts.montserrat(
@@ -150,44 +113,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class _AnimatedBuilder extends StatelessWidget {
-  const _AnimatedBuilder({
-    Key? key,
-    required this.animationController,
-    required this.translateAnimation,
-    required this.scaleAnimation,
-    required this.rotateAnimation,
-    required this.icon,
-  }) : super(key: key);
-
-  final AnimationController animationController;
-  final Animation<Offset> translateAnimation;
-  final Animation<double> scaleAnimation;
-  final Animation<double> rotateAnimation;
-  final IconData icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (context, child) => Transform.translate(
-        offset: translateAnimation.value,
-        child: Transform.scale(
-          scale: scaleAnimation.value,
-          child: Transform.rotate(
-            angle: rotateAnimation.value,
-            child: Icon(
-              icon,
-              size: sizes.width(context) / 2,
-              color: Colors.white,
-            ),
-          ),
         ),
       ),
     );
