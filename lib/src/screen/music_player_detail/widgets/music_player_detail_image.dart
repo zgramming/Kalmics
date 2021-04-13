7adcc1bf753c656c46976452885512e060b5fad0
Zeffry Reynando <zeffry.reynando@gmail.com>
2021-04-13 07:58:07 +0700

    Change Animation to Opacity & Prevent Memory leak

--- lib/src/screen/music_player_detail/widgets/music_player_detail_image.dart
+++ lib/src/screen/music_player_detail/widgets/music_player_detail_image.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:global_template/global_template.dart';
 
@@ -12,57 +11,65 @@ class MusicPlayerDetailImage extends StatefulWidget {
 
 class _MusicPlayerDetailImageState extends State<MusicPlayerDetailImage>
     with SingleTickerProviderStateMixin {
-  late AnimationController _sizeController;
-  late Animation<double> _sizeAnimation;
+  late AnimationController _controller;
+  late Animation<double> _opacity;
   @override
   void initState() {
-    SchedulerBinding.instance?.addPostFrameCallback((_) {
-      context.read(globalSizeAnimationController).state = _sizeController;
-    });
-    _sizeController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    _sizeAnimation = Tween<double>(begin: 0.0, end: 1.0)
-        .animate(CurvedAnimation(parent: _sizeController, curve: Curves.fastOutSlowIn));
-    _sizeController.forward();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 2500));
 
+    _opacity = Tween<double>(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(curve: Curves.fastOutSlowIn, parent: _controller));
+    _controller.forward();
     super.initState();
   }
 
   @override
   void dispose() {
-    _sizeController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, watch, child) {
-        final _currentSong = watch(currentSongProvider.state);
-        final artwork = _currentSong.song.artwork;
-        return SizeTransition(
-          sizeFactor: _sizeAnimation,
-          axis: Axis.horizontal,
-          child: artwork == null
-              ? Image.asset(
-                  appConfig.fullPathImageAsset,
-                  fit: BoxFit.cover,
-                  width: sizes.width(context),
-                )
-              : Image.memory(
-                  artwork,
-                  fit: BoxFit.cover,
-                  height: sizes.height(context),
-                  width: sizes.width(context),
-                  errorBuilder: (context, error, stackTrace) {
-                    return Image.asset(
-                      appConfig.fullPathImageAsset,
-                      fit: BoxFit.cover,
-                      width: sizes.width(context),
-                    );
-                  },
-                ),
-        );
-      },
+    return AnimatedBuilder(
+      animation: _opacity,
+      builder: (context, child) => Opacity(
+        opacity: _opacity.value,
+        child: Consumer(
+          builder: (context, watch, child) {
+            final _currentSong = watch(currentSongProvider.state);
+            final artwork = _currentSong.song.artwork;
+            return artwork == null
+                ? Image.asset(
+                    appConfig.fullPathImageAsset,
+                    fit: BoxFit.cover,
+                    width: sizes.width(context),
+                  )
+                : Image.memory(
+                    artwork,
+                    fit: BoxFit.cover,
+                    height: sizes.height(context),
+                    width: sizes.width(context),
+                    errorBuilder: (context, error, stackTrace) {
+                      return Image.asset(
+                        appConfig.fullPathImageAsset,
+                        fit: BoxFit.cover,
+                        width: sizes.width(context),
+                      );
+                    },
+                  );
+          },
+        ),
+      ),
     );
+    // return AnimatedOpacity(
+    //   duration: const Duration(seconds: 10),
+    //   opacity: ,
+    //   child: Center(
+    //     child: FlutterLogo(
+    //       size: 300,
+    //     ),
+    //   ),
+    // );
   }
 }
